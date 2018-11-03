1cdde98957d4e0c404385c21d368b439e7c928aa
Anthony Cantellano <acantellano1996@gmail.com>
2018-11-03 09:35:54 -0700

    Testing out compositional model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', null, 'Hello, world!')
+const tasks = ['Take out the trash', 'Wash car', 'change oil'];
 
-ReactDOM.render(element, document.getElementById('root'))
+class List extends Component {
+  render() {
+    return (
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Task List</h1>
+      </div>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Title /> <List /> <List /> <List />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
